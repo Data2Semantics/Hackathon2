c596227371876f8626c9ac403aab26799fac0897
Peter Bloem <p@peterbloem.nl>
2013-09-12 14:34:38 +0200

    ...

--- python2platform/data2semantics/python2platform.py
+++ python2platform/data2semantics/python2platform.py
@@ -17,29 +17,60 @@ The expected procedure is something like this:
 @author: Peter
 '''
 
+import subprocess as sp
+import os
+
+# The directory of the platform's pom.xml
+PLATFORM_DIR = ""
+
 def applicable(mimetype):
     '''
       Return a list of descriptors of workflows. Each entry is a triple of the 
       form: (identifier, human-readable name, description)
     '''
-    return [
-        ('test1', 'Test workflow 1', 'This workflows tests something') 
-        ('test2', 'Test workflow 2', 'This workflow does another thing')
-    ]
+    
+    return [('test1', 'Test workflow 1', 'This workflows tests something')]
 
-def run(workflow_identifier, location, data):
+def run(identifier, location, datafile):
     '''
       Starts a workflow in a separate process. If there is already a workflow
       running in the given location, the function throws an exception
     ''' 
+    # Read the workflow file into a string
+    workflowFile = open('./'+identifier+'.yaml', 'r')
+    workflowYAML = workflowFile.read()
+    
+    # Insert the filename
+    workflowYAML.substitute(file=datafile)
+    
+    # Write the workflow file to the location
+    wfFileOut = open(location+'/workflow.yaml', 'w')
+    wfFileOut.write(workflowYAML)
+    wfFileOut.close()
+    
+    # Change directory to the platform dir
+    myDirectory = os.getcwd() # remember our original working directory
+    os.chdir(PLATFORM_DIR)
+    
+    # Call the platform
+    args = ["mvn", "exec:java", "-Dexec:mainClass='org.data2semantics.platform.run.Run'", "-Dexec:args='--output $location $location/workflow.yaml'"]
+    sp.Popen(args) # run in the background
+    
+    os.chdir(myDirectory)
+    
     
 def status(location):
     '''
       Returns the current status of a workflow runnning in the given location 
       (a directory). The status is represented by one of the strings 
           'running'
-          'no workflow found'
+          'no workflow'
           'error'
           'finished'
     '''
-    
+    if os.path.exists(location  + '/status.running'):
+        return 'running'
+    if os.path.exists(location  + '/status.finished'):
+        return 'finished'
+    return 'no workflow' 
+

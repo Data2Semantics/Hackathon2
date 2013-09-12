06333b023a6b28616d81bd7ac373f774718177f1
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-09-12 15:11:33 +0200

    First things first

---
+++ backend/src/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+# Make sure we have an absolute path to the template dir
+TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
+
+# Intialize the Flask Appliation
+app = Flask(__name__, template_folder = TEMPLATE_FOLDER)
+
+
+app.debug = True
+
+import views

6fe660f2b8a473c4753b898c5ecead1fd3f8b1c9
Laurens Rietveld <laurens.rietveld@gmail.com>
2013-09-12 16:20:27 +0200

    impl landing page

---
+++ frontend/assets/js/hackathon.js
@@ -0,0 +1,7 @@
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = [maxWidth / srcWidth, maxHeight / srcHeight ];
+    ratio = Math.min(ratio[0], ratio[1]);
+
+    return { width:srcWidth*ratio, height:srcHeight*ratio };
+ }

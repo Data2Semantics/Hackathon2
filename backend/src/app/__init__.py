22115dbde13602980fe6089877ab48e3b2cdbcdf
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2013-09-19 16:32:28 +0200

    Global constants were moved to workflow. Added (and commented) database stuff

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,21 +1,26 @@
 from flask import Flask
+#from flask.ext.sqlalchemy import SQLAlchemy
+
 import os
 
 # Make sure we have an absolute path to the template dir
 TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-
+SCRATCH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scratch')
 
 # STATIC_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 
 # Intialize the Flask Appliation
 app = Flask(__name__, template_folder = TEMPLATE_FOLDER)
-
-
-SCRATCH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scratch')
-
-WORKFLOW_RESULTS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
-
-
 app.debug = True
+
+app.secret_key = 'jedenkttochzekernietdatjedezesecretkeykanradenhe?'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///goldendemo.db'
+#db = SQLAlchemy(app)
+#
+#if not os.path.exists('goldendemo.db'):
+#    app.logger.warn("Database does not exist, will create one")
+#    db.create_all()
+
 
 import views

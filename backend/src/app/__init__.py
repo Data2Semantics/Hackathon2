dd582dccb422438a15e62f37e0a5a20deec0e085
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-10-01 14:37:30 +0200

    Added escapejs filter for Jinja

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -23,4 +23,40 @@ app.secret_key = 'jedenkttochzekernietdatjedezesecretkeykanradenhe?'
 #    db.create_all()
 
 
+## JINJA2 Filters
+_base_js_escapes = (
+    ('\\', r'\u005C'),
+    ('\'', r'\u0027'),
+    ('"', r'\u0022'),
+    ('>', r'\u003E'),
+    ('<', r'\u003C'),
+    ('&', r'\u0026'),
+    ('=', r'\u003D'),
+    ('-', r'\u002D'),
+    (';', r'\u003B'),
+    ('/', '_'), # Added by RH
+    (' ', '_'), # Added by RH
+    ('.', '_'), # Added by RH
+    (u'\u2028', r'\u2028'),
+    (u'\u2029', r'\u2029')
+)
+
+# Escape every ASCII character with a value less than 32.
+_js_escapes = (_base_js_escapes +
+               tuple([('%c' % z, '\\u%04X' % z) for z in range(32)]))
+
+# escapejs from Django: https://www.djangoproject.com/
+@app.template_filter('escapejs')
+def escapejs(value):
+    """Hex encodes characters for use in JavaScript strings."""
+    if not isinstance(value, basestring):
+        value = str(value)
+
+    for bad, good in _js_escapes:
+        value = value.replace(bad, good)
+
+    return value
+
+
+
 import views
